4e14a92b63d15daa0f4daa0fea8feda04b2858a4
sceley <1538306377@qq.com>
2018-03-18 19:26:30 +0800

    modify .gitignore

---
+++ client/src/components/Person/MyReserve.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+export default class MyReserve extends Component {
+    render () {
+        return (
+            <div className="MyReserve"></div>
+        );
+    }
+}
