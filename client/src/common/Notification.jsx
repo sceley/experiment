21f960b7223a449a4af6765131ea09ea9bed3d0d
sceley <1538306377@qq.com>
2018-04-17 12:45:34 +0800

    modify Notification UI

--- client/src/common/Notification.jsx
+++ client/src/common/Notification.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { notification, Icon, Button } from 'antd';
 import config from '../config';
 import logo from '../logo.png';
+import moment from 'moment';
 import Markdown from 'markdown-it';
 const md = new Markdown();
 export default class Notify extends Component {
@@ -34,14 +35,27 @@ export default class Notify extends Component {
                 </div>
                 <div style={{ width: 800, margin: '30px auto' }} className="artile">
                     <h1>{this.state.notification.title}</h1>
+                    <ul className="extra-list">
+                        <li className="extra-list-item">
+                            <em>时间：</em>
+                            <em>
+                                {moment(this.state.notification.createAt).format("YYYY-MM-DD HH:mm:ss")}
+                            </em>
+                        </li>
+                        <li className="extra-list-item">
+                            <em>作者：</em>
+                            <em>{this.state.notification.author}</em>
+                        </li>
+                    </ul>
                     <div
                         dangerouslySetInnerHTML={{
                             __html: md.render(this.state.notification.msg || '')
                         }}
+                        style={{ marginTop: 20 }}
                     >
                     </div>
                 </div>
-                <div style={{  padding: '20px 0', fontSize: 20 , textAlign: 'center' }}>
+                <div style={{  padding: '20px 0 100px 0', fontSize: 20 , textAlign: 'center' }}>
                     <Button onClick={this.handleRollback}>
                         返回<Icon style={{}} type="rollback" />
                     </Button>
