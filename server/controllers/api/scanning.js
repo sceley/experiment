abb62a6a072746e1bf0826df896d631c42c4f86a
sceley <1538306377@qq.com>
2018-05-30 02:18:51 +0800

    support scanning time format

--- server/controllers/api/scanning.js
+++ server/controllers/api/scanning.js
@@ -1,10 +1,11 @@
 const db = require('../../model/db');
 const send = require('../tcp/socket').send;
+const moment = require('moment');
 const convert_to_str = require('../../common/convert').convert_to_str;
 exports.scanningOpen = async (req, res) => {
     try {
         const account = req.session.user.account;
-        const hour = ( new Date() ).getHours() + ( new Date() ).getMinutes() / 60;
+        const hour = moment().format("HH:mm:ss");
         const reserve = await new Promise((resolve, reject) => {
             const sql = 'select id as NUM, exp_id as EXP, seat as TAB, user_id as account from Reserve where start<=? and end>? and user_id=?';
             db.connection.query(sql, [hour, hour, account], (err, reserves) => {
