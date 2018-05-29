d7b8d692e56a02ac30ecbdf8e21c48baf1cfcf14
sceley <1538306377@qq.com>
2018-05-29 12:44:17 +0800

    fix sql connect

--- server/model/db.js
+++ server/model/db.js
@@ -1,8 +1,24 @@
 const mysql = require('mysql');
 const config = require('../config');
-const connection = mysql.createConnection(config.db);
-
-connection.connect();
+let connection;
+function handleDisconnect() {
+	connection = mysql.createConnection(config.db);
+	connection.connect(err => {
+		if (err) {
+			console.log('error when connecting to db:', err);
+			setTimeout(handleDisconnect, 2000);
+		}
+	});
+	connection.on('error', err => {
+		console.log('db error', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
+};
+handleDisconnect();
 
 const table1 = `create table if not exists Admin (
 				account varchar(15) unique,
